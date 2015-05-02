b2e2e2ec6441d8dd30c1ccbbe4c9c32de2839917
Manoj Venkata Tutika <manoj.tutika@chronus.com>
2015-05-02 19:24:31 +0530

    Init Repo.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HachiApp::Application.config.secret_token = '8645c44f58e1f0e70b78e6d630c87f5718645683a3e93660a26b2b0b5f054834770ce9879ca4992ab0e25357c52cd16a152b8f9b5bfe4a6a3ace50ec70e9ec98'
